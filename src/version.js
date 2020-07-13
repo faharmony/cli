87d4c11005d78537fa3d28bafeab35c7fb1245d1
Siddhant Gupta <me@guptasiddhant.com>
2020-07-13 19:52:33 +0300

    Remove pacakge info from version

--- src/version.js
+++ src/version.js
@@ -23,25 +23,25 @@ const {
 } = require("./utilities");
 
 /** Get information about installed packages */
-const harmonyInfo = async () => {
-  console.log(color(`Finding harmony libraries in this project...`));
-  let count = 0;
-  await asyncForEach(
-    mainPackages.concat(commonPackages),
-    /** @param {Package} pkg */
-    (pkg) => {
-      const pkgInfo = checkPackageInfo(pkg.name);
-      if (pkgInfo) {
-        count++;
-        console.log(
-          color(`[${pkgInfo.tag}] ${bold(pkgInfo.name)}@${pkgInfo.version}`)
-        );
-      }
-    }
-  );
-  if (count === 0) { console.log(error(`No harmony libraries found in this project.`)); getHelp(); }
-  else console.log(color(`${count} harmony libraries found in this project.`));
-};
+// const harmonyInfo = async () => {
+//   console.log(color(`Finding harmony libraries in this project...`));
+//   let count = 0;
+//   await asyncForEach(
+//     mainPackages.concat(commonPackages),
+//     /** @param {Package} pkg */
+//     (pkg) => {
+//       const pkgInfo = checkPackageInfo(pkg.name);
+//       if (pkgInfo) {
+//         count++;
+//         console.log(
+//           color(`[${pkgInfo.tag}] ${bold(pkgInfo.name)}@${pkgInfo.version}`)
+//         );
+//       }
+//     }
+//   );
+//   if (count === 0) { console.log(error(`No harmony libraries found in this project.`)); getHelp(); }
+//   else console.log(color(`${count} harmony libraries found in this project.`));
+// };
 
 /** Check version of installed harmony packages */
 const harmonyVersion = async () => {
@@ -54,7 +54,7 @@ const harmonyVersion = async () => {
         }).`
       )
     );
-    await harmonyInfo();
+    // await harmonyInfo();
   }
 
   else {
